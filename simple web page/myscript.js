84f50cda0dbc3769d2d4ed77301e3b35751cb0da
Ravikiran Yadava <ravikiransjce@gmail.com>
2017-10-04 21:06:00 -0400

    modified the myscript.js

--- simple web page/myscript.js
+++ simple web page/myscript.js
@@ -1,3 +1,15 @@
 var year = 2017;
 //javascript is a weekly typed langauge
-//Java is a strongly typed langauge, we have to specify the type of the variables
+//Java is a strongly typed langauge, we have to specify the type of the variables like int, String, boolean
+
+// conditions
+
+var a = 50, b = 23;
+
+if(a<50 || b >10){
+	console.log("true");	
+}else{
+	console.log("false");	
+}
+
+//created the branch wed_04_10
